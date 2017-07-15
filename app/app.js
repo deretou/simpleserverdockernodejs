964ca7e76a9b7b9b390bae553390d1c8a0449e13
deretou <deretou@gmail.com>
2017-07-15 00:28:18 -0400

    server sources code
    server sources code

---
+++ app/app.js
@@ -0,0 +1,12 @@
+
+var http = require('http').createServer(handler);
+
+function handler (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello AppDirect Platform!'); 
+ res.end();
+}
+
+http.listen(3007, function(){
+  console.log('AppDirect Platform server listening on port 3007!');
+});
